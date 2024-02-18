cbc8817c7bc1a1143a1cbe28d72f3a8efef49054
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-02-18 16:38:06 +0300

    Added equivalentLoadFromFreeLocalLoad()

--- files/equivalent_load.c
+++ files/equivalent_load.c
@@ -1,35 +1,131 @@
 // Консольное приложение для определения эквивалентной нагрузки на балку
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <math.h>
 #include <windows.h>
 
-void equivalentLoadFromLocalLoad(double localLoad, double beamLength);
+double equivalentLoad = 0.0;   // значение эквивалентной нагрузки
+double offNodeLoadEqual = 0.0; // значение внеузловой нагрузки равной по значению слева и справа
+double offNodeLoadA = 0.0;     // значение внеузловой нагрузки у левой опоры балки
+double offNodeLoadB = 0.0;     // значение внеузловой нагрузки у правой опоры балки
+double beamLength = 0.0;       // пролет балки
+double localLoad = 0.0;        // значение сосредоточенной силы
+double sizeLengthA = 0.0;      // привязка внешнего воздействия к левой опоре
+double sizeLengthB = 0.0;      // привязка внешнего воздействия к правой опоре
 
+int menu(void);                             // меню программы
+void equivalentLoadFromLocalLoad(void);     // эквивалентная от сосредоточенной силы в середине пролета
+void equivalentLoadFromFreeLocalLoad(void); // эквивалентная от сосредоточенной силы в середине пролета
+void help(void);                            // справка по работе с программой
 
 int main()
 {
     SetConsoleCP(1251); SetConsoleOutputCP(1251);
-    double beamLength = 0.0;     // пролет балки
-    double localLoad = 0.0;      // значение сосредоточенной силы
-    //double equivalentLoad = 0.0; // значение эквивалентной нагрузки
-    //double offNodeLoad = 0.0;    // значение внеузловой нагрузки
 
+    int choice = 0;
+
+    while(1)
+    {
+        choice = menu();
+        switch(choice)
+        {
+            case 'S':
+            equivalentLoadFromLocalLoad();
+            break;
+        case 'A':
+            equivalentLoadFromFreeLocalLoad();
+            break;
+        case 'D':
+            // резерв
+            break;
+        case 'I':
+            // резерв
+            break;
+        case 'H':
+            help();
+            break;
+        case 'Q':
+            puts("Всего доброго!");
+            exit(EXIT_SUCCESS);
+            break;
+        default:
+            puts("Некорректный ввод. Введите букву английского алфавита.");
+            break;
+        }
+
+    }
+    system("pause");
+    return 0;
+}
+
+// меню программы
+int menu(void)
+{
+    int ch = 0;
+    puts("Меню:");
+    printf("S - сосредоточенная сила в середине пролета,\nA - сосредоточенная сила в произвольном месте балки, \
+\nD - резерв,\nI - резерв,\nH - справка,\nQ - выход;\n");
+
+    ch = getch(); // ожидает нажатия клавиши, после которого она немедленно возвращает значение
+
+    putchar(ch); // вывод символа в стандартный поток вывода
+    putch('\n'); // функция производит вывод прямо на экран
+    ch = toupper(ch); // преобразование строчных букв в прописные
+    // после выбора действия в меню сперва очищается экран
+     if(ch == 'S' || ch == 'A'|| ch == 'D'|| ch == 'I'|| ch == 'H'|| ch == 'Q'||
+        ch != 'S'|| ch != 'A'|| ch != 'D'|| ch != 'I'|| ch != 'H' || ch != 'Q')
+    {
+        system("cls");
+    }
+    return ch;
+}
+
+// эквивалентная от сосредоточенной силы в середине пролета
+void equivalentLoadFromLocalLoad()
+{
     puts("Введите длину балки, м:");
     scanf("%Lf", &beamLength);
     fflush(stdin);
     puts("Введите значение сосредоточенной силы, т:");
     scanf("%Lf", &localLoad);
     fflush(stdin);
-
-    equivalentLoadFromLocalLoad(localLoad, beamLength);
-
-    return 0;
+    equivalentLoad = 2 * localLoad / beamLength;
+    offNodeLoadEqual = 0.5 * (localLoad - equivalentLoad * beamLength);
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("Результат вычисления:\n");
+    printf("Эквивалентная нагрузка q = %.2f т\\м\n", equivalentLoad);
+    printf("Внеузловая нагрузка F1 = F2 = %.2f т\n", offNodeLoadEqual);
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 }
 
-void equivalentLoadFromLocalLoad(double localLoad, double beamLength)
+void equivalentLoadFromFreeLocalLoad()
 {
-    double equivalentLoad = 2 * localLoad / beamLength;
-    double offNodeLoad = 0.5 * (localLoad - equivalentLoad * beamLength);
-    printf("Эквивалентная нагрузка: %.2f т\\м\n", equivalentLoad);
-    printf("Внеузловая нагрузка: %.2f т\n", offNodeLoad);
+    puts("Введите длину балки, м:");
+    scanf("%Lf", &beamLength);
+    fflush(stdin);
+    puts("Введите значение привязки сосредоточенной силы к левой опоре балки, м");
+    scanf("%Lf", &sizeLengthA);
+    fflush(stdin);
+    puts("Введите значение сосредоточенной силы, т:");
+    scanf("%Lf", &localLoad);
+    fflush(stdin);
+    sizeLengthB = beamLength - sizeLengthA;
+    equivalentLoad = (8 * localLoad * sizeLengthA * sizeLengthB) / pow(beamLength, 3);
+    offNodeLoadA = (localLoad * sizeLengthB) / beamLength - 0.5 * equivalentLoad * beamLength;
+    offNodeLoadB = -(localLoad * sizeLengthA) / beamLength - 0.5 * equivalentLoad * beamLength;
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("Результат вычисления:\n");
+    printf("Эквивалентная нагрузка q = %.2f т\\м\n", equivalentLoad);
+    printf("Внеузловая нагрузка F1 = %.2f т\n", offNodeLoadA);
+    printf("Внеузловая нагрузка F2 = %.2f т\n", offNodeLoadB);
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+}
+
+// справка по работе с программой
+void help(void)
+{
+    printf("\nСправка:\n");
+    printf("* Программа предназначена для определения эквивалентной нагрузки.\n");
+    printf("* Знак \"минус\" внеузловой нагрузки означает её направление вверх.\n");
 }

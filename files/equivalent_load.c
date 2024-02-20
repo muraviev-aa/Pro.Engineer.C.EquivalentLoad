604ecff1e3e01a54fa903ab70ee39a47efd6f696
Муравьев Алексей <103278793+muraviev-aa@users.noreply.github.com>
2024-02-20 21:05:46 +0300

    Added equivalentLoadFromTriangularLoad()

--- files/equivalent_load.c
+++ files/equivalent_load.c
@@ -5,19 +5,21 @@
 #include <math.h>
 #include <windows.h>
 
-double equivalentLoad = 0.0;   // значение эквивалентной нагрузки
-double offNodeLoadEqual = 0.0; // значение внеузловой нагрузки равной по значению слева и справа
-double offNodeLoadA = 0.0;     // значение внеузловой нагрузки у левой опоры балки
-double offNodeLoadB = 0.0;     // значение внеузловой нагрузки у правой опоры балки
-double beamLength = 0.0;       // пролет балки
-double localLoad = 0.0;        // значение сосредоточенной силы
-double sizeLengthA = 0.0;      // привязка внешнего воздействия к левой опоре
-double sizeLengthB = 0.0;      // привязка внешнего воздействия к правой опоре
+double equivalentLoad = 0.0;        // значение эквивалентной нагрузки
+double offNodeLoadEqual = 0.0;      // значение внеузловой нагрузки равной по значению слева и справа
+double offNodeLoadA = 0.0;          // значение внеузловой нагрузки у левой опоры балки
+double offNodeLoadB = 0.0;          // значение внеузловой нагрузки у правой опоры балки
+double beamLength = 0.0;            // пролет балки
+double localLoad = 0.0;             // значение сосредоточенной силы
+double triangularDistributed = 0.0; // значение треугольной распределенной нагрузки
+double sizeLengthA = 0.0;           // привязка внешнего воздействия к левой опоре
+double sizeLengthB = 0.0;           // привязка внешнего воздействия к правой опоре
 
 int menu(void);                              // меню программы
 void equivalentLoadFromLocalLoad(void);      // эквивалентная от сосредоточенной силы в середине пролета
 void equivalentLoadFromFreeLocalLoad(void);  // эквивалентная от сосредоточенной силы в произвольном месте балки
 void equivalentLoadFromSimTwoLocalLoad(void);// эквивалентная от двух симметричных сосредоточенных нагрузки
+void equivalentLoadFromTriangularLoad(void); // эквивалентная от треугольной нагрузки на всей длине балки
 void help(void);                             // справка по работе с программой
 
 int main()
@@ -41,7 +43,7 @@ int main()
             equivalentLoadFromSimTwoLocalLoad();
             break;
         case 'I':
-            // резерв
+            equivalentLoadFromTriangularLoad();
             break;
         case 'H':
             help();
@@ -65,8 +67,10 @@ int menu(void)
 {
     int ch = 0;
     puts("Меню:");
-    printf("S - сосредоточенная сила в середине пролета,\nA - сосредоточенная сила в произвольном месте балки, \
-\nD - две равные по значению симметрично расположенные сосредоточенные силы,\nI - резерв,\nH - справка,\nQ - выход;\n");
+    printf("S - сосредоточенная сила в середине пролета, \
+\nA - сосредоточенная сила в произвольном месте балки, \
+\nD - две равные по значению симметрично расположенные сосредоточенные силы, \
+\nI - треугольная нагрузка на всей длине балки,\nH - справка,\nQ - выход;\n");
 
     ch = getch(); // ожидает нажатия клавиши, после которого она немедленно возвращает значение
 
@@ -101,7 +105,7 @@ void equivalentLoadFromLocalLoad()
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("Результат вычисления:\n");
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-    printf("Эквивалентная нагрузка q = %.2f т\\м\n", equivalentLoad);
+    printf("Эквивалентная нагрузка q = %.2f т/м\n", equivalentLoad);
     printf("Внеузловая нагрузка F1 = F2 = %.2f т\n", offNodeLoadEqual);
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 }
@@ -131,7 +135,7 @@ void equivalentLoadFromFreeLocalLoad()
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("Результат вычисления:\n");
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-    printf("Эквивалентная нагрузка q = %.2f т\\м\n", equivalentLoad);
+    printf("Эквивалентная нагрузка q = %.2f т/м\n", equivalentLoad);
     printf("Внеузловая нагрузка F1 = %.2f т\n", offNodeLoadA);
     printf("Внеузловая нагрузка F2 = %.2f т\n", offNodeLoadB);
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
@@ -160,8 +164,34 @@ void equivalentLoadFromSimTwoLocalLoad()
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     printf("Результат вычисления:\n");
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-    printf("Эквивалентная нагрузка q = %.2f т\\м\n", equivalentLoad);
+    printf("Эквивалентная нагрузка q = %.2f т/м\n", equivalentLoad);
     printf("Внеузловая нагрузка F1 = F2 = %.2f т\n", offNodeLoadEqual);
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+}
+
+// эквивалентная от треугольной нагрузки на всей длине балки
+void equivalentLoadFromTriangularLoad()
+{
+    puts("Введите длину балки, м:");
+    scanf("%Lf", &beamLength);
+    fflush(stdin);
+    puts("Введите значение треугольной нагрузки по всей балке, т/м:");
+    scanf("%Lf", &triangularDistributed);
+    fflush(stdin);
+    equivalentLoad = 0.512 * triangularDistributed;
+    offNodeLoadA = beamLength * (triangularDistributed / 6 - 0.5 * equivalentLoad);
+    offNodeLoadB = beamLength * (triangularDistributed / 3 - 0.5 * equivalentLoad);
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("Исходные данные:\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("Длина балки = %.2f м\n", beamLength);
+    printf("Значение треугольной нагрузки по всей балке = %.2f т/м\n", triangularDistributed);
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("Результат вычисления:\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("Эквивалентная нагрузка q = %.2f т/м\n", equivalentLoad);
+    printf("Внеузловая нагрузка F1 = %.2f т\n", offNodeLoadA);
+    printf("Внеузловая нагрузка F2 = %.2f т\n", offNodeLoadB);
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 }
 
